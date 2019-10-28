21f6ef335d9045ca21c0ebea760d5d551dd17e8a
TrynowPht <56600043+TrynowPht@users.noreply.github.com>
2019-10-28 09:12:59 -0500

    Add files via upload
    Cambios en la generación de Bloque Mena de Adamantium:
    
    Ahora el Bloque se genera entre las alturas 10 y 20.
    Se pueden generar Menas entre 2 a 4 Bloques juntos.
    Probabilidad disminuida a 8.

--- src/main/java/com/trynow/adamod/world/ModWorldGen.java
+++ src/main/java/com/trynow/adamod/world/ModWorldGen.java
@@ -24,7 +24,7 @@ public class ModWorldGen implements IWorldGenerator{
 	 
 	private void generateOverworld(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) 
 	{
-		generateOre(BlockInit.ADAMANTITE_ORE.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 16, 64, random.nextInt(7) + 4, 18);
+		generateOre(BlockInit.ADAMANTITE_ORE.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 10, 20, random.nextInt(4) + 2, 8);
 	}	
 	
 	private void generateOre(IBlockState ore, World world, Random random, int x, int z, int minY, int maxY, int size, int chances) 
